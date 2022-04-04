4fa13e8a508d42b53d365e790d2802d0fc14ba74
Hrittik <hrittikgarain@gmail.com>
2022-04-04 21:57:00 +0530

    c7 : delete campground

--- app.js
+++ app.js
@@ -70,6 +70,14 @@ app.put('/campgrounds/:id' , async(req , res) =>{
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+
+// Deleting a campground
+app.delete('/campgrounds/:id' , async(req , res) =>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+})
+
 app.listen(3000 , () => {
     console.log("Serving port at 3000")
 })
