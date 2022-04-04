505ef567dee81addf9fe9bac88829cdb8e54e74d
Hrittik <hrittikgarain@gmail.com>
2022-04-04 21:48:05 +0530

    c6 : edit and update data in the data base

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground.js');
-const { shallowCopy } = require('ejs/lib/utils');
 /**
  * ***********************************************
  *                DATABASE WORK
@@ -28,6 +28,7 @@ app.set('views engine' , 'ejs');
 app.set('views' , path.join(__dirname , 'views'))
 
 app.use(express.urlencoded({ extended : true }))
+app.use(methodOverride('_method'));
 
 app.get('/' , (req ,res)=>{
     res.render('home.ejs')
@@ -37,7 +38,9 @@ app.get('/campgrounds' , async (req ,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs' , {campgrounds})
 })
-// Create new Campground
+
+
+// Create new Campground 
 app.get('/campgrounds/new' , (req , res) => {
     res.render('campgrounds/new.ejs');
 })
@@ -47,12 +50,25 @@ app.post('/campgrounds' , async (req , res) => {
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 })
-// for reading data
+
+//  Reading data From Database
 app.get('/campgrounds/:id' , async( req , res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show.ejs' , { campground });
 })
 
+
+// Editing existing data in the database
+app.get('/campgrounds/:id/edit' , async( req , res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit.ejs' , { campground });
+})
+
+app.put('/campgrounds/:id' , async(req , res) =>{
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id , { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000 , () => {
     console.log("Serving port at 3000")
