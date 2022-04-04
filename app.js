eb749b91df3ae5f2417e563d35860be905ff620c
Hrittik <hrittikgarain@gmail.com>
2022-04-04 21:23:09 +0530

    c5 : creating new data in the data base

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 app.set('views engine' , 'ejs');
 app.set('views' , path.join(__dirname , 'views'))
 
-
+app.use(express.urlencoded({ extended : true }))
 
 app.get('/' , (req ,res)=>{
     res.render('home.ejs')
@@ -37,7 +37,17 @@ app.get('/campgrounds' , async (req ,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs' , {campgrounds})
 })
+// Create new Campground
+app.get('/campgrounds/new' , (req , res) => {
+    res.render('campgrounds/new.ejs');
+})
 
+app.post('/campgrounds' , async (req , res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+// for reading data
 app.get('/campgrounds/:id' , async( req , res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show.ejs' , { campground });
